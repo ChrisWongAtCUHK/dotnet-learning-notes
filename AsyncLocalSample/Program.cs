2d4116c1d1df032e1f02cba2957d8fcd1ea9cfe0
Chris Wong <chris.wong.ksl@gmail.com>
2026-05-01 11:38:51 +0800

    五個 TestNumAdder() 非同步執行卻共用同一份會記憶 A, B 值的 NumberAdder 物件，一定會覆寫來覆寫去

--- AsyncLocalSample/Program.cs
+++ AsyncLocalSample/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿await Demo.RunAsync();
+
+class NumberAdder
+{
+    public int A;
+    public int B;
+    public override string ToString() => $"{A} + {B} = {A + B}";
+}
+
+static class Demo
+{
+    private static readonly Random rand = new();
+    static NumberAdder adder = new();
+
+    public static async Task RunAsync()
+    {
+        await Task.WhenAll(
+            TestNumAdder("Alice", 1, 2),
+            TestNumAdder("Bob", 3, 4),
+            TestNumAdder("Charlie", 5, 6),
+            TestNumAdder("Diana", 7, 8),
+            TestNumAdder("Eve", 3, 8)
+        );
+    }
+
+    static async Task TestNumAdder(string user, int a, int b)
+    {
+        await Task.Delay(rand.Next(5, 700));
+        adder.A = a;
+        await Task.Delay(rand.Next(5, 700));
+        adder.B = b;
+        Console.WriteLine($" - {user,-10} Test {a} + {b} / Result: {adder, -12} T:{Thread.CurrentThread.ManagedThreadId}");
+    }
+}
