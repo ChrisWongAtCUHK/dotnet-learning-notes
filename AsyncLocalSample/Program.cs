4719300aefa2eb25d084275f275ace4cb3accae8
Chris Wong <chris.wong.ksl@gmail.com>
2026-05-01 11:40:49 +0800

    async 不能用 lock，改用 SemaphoreSlim 實現互斥鎖

--- AsyncLocalSample/Program.cs
+++ AsyncLocalSample/Program.cs
@@ -23,12 +23,22 @@ static class Demo
         );
     }
 
+    // async 不能用 lock，改用 SemaphoreSlim 實現互斥鎖
+    private static readonly SemaphoreSlim semaphore = new(1, 1);
     static async Task TestNumAdder(string user, int a, int b)
     {
-        await Task.Delay(rand.Next(5, 700));
-        adder.A = a;
-        await Task.Delay(rand.Next(5, 700));
-        adder.B = b;
-        Console.WriteLine($" - {user,-10} Test {a} + {b} / Result: {adder, -12} T:{Thread.CurrentThread.ManagedThreadId}");
+        await semaphore.WaitAsync();
+        try
+        {
+            await Task.Delay(rand.Next(5, 700));
+            adder.A = a;
+            await Task.Delay(rand.Next(5, 700));
+            adder.B = b;
+            Console.WriteLine($" - {user,-10} Test {a} + {b} / Result: {adder, -12} T:{Thread.CurrentThread.ManagedThreadId}");
+        }
+        finally
+        {
+            semaphore.Release();
+        }
     }
 }
